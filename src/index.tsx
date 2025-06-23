1b252535b1615b50ef161ca6cd9468f8cee3d03e
Viktoriia <loremlisumdol@gmail.com>
2025-06-23 20:33:32 +0700

    chore: add new tasks

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./s1-main/App";
+import { Provider } from "react-redux";
+import store from "./s2-homeworks/hw10/bll/store";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    {/*для дз 10*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
